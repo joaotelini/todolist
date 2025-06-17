44f22ce9c8c81acb359506b33159a34e52ac4fc2
João Pedro Telini <joaotelini03@gmail.com>
2025-06-17 13:15:07 -0300

    add icons to delete or mark as completed in the task list

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { Task } from "@/types/TaskType";
 import { getTasksData } from "@/data/TaskData";
+import { Trash2, BadgeCheck } from "lucide-react";
 
 export const TaskList = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
@@ -21,14 +22,30 @@ export const TaskList = () => {
   }, []);
 
   return (
-    <div className="mt-10">
+    <div className="mt-5 w-100">
       {tasks.length === 0 ? (
         <p className="text-center">Nenhuma tarefa encontrada.</p>
       ) : (
-        <ul className="rounded p-4 shadow-lg">
+        <ul className="flex flex-col gap-2 rounded p-4 shadow-lg">
           {tasks.map((task) => (
-            <li key={task.id} className="p-2 text-white">
-              {task.taskName}
+            <li
+              key={task.id}
+              className="flex items-center justify-between gap-5 bg-zinc-800 px-4 py-2 rounded"
+            >
+              <span>{task.taskName}</span>
+
+              <div className="flex items-center gap-3">
+                <BadgeCheck
+                  className="w-5 h-5 cursor-pointer"
+                  color="#3de1de"
+                  strokeWidth={0.75}
+                />
+                <Trash2
+                  className="w-5 h-5 cursor-pointer"
+                  color="#bd2828"
+                  strokeWidth={1.5}
+                />
+              </div>
             </li>
           ))}
         </ul>

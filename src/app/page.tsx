850d8d32913f5d769a97fb8f02af805eda736dc4
João Pedro Telini <joaotelini03@gmail.com>
2025-06-18 23:03:03 -0300

    Remove unnecessary TaskInput import and simplify JSX structure in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import { TaskInput } from "@/components/TaskInput";
 import { TaskList } from "@/components/TaskList";
 
 export default function Home() {
   return (
     <div className="flex text-white min-h-screen items-center flex-col justify-center bg-gradient-to-br from-stone-800 via-zinc-700 to-slate-900 ">
-      <h1 className="mb-10 text-5xl font-bold text-center">
-        Lista de Tarefas
-      </h1>
-      <div>
-        <TaskInput />
-      </div>
+      <h1 className="mb-10 text-5xl font-bold text-center">Lista de Tarefas</h1>
       <div>
         <TaskList />
       </div>

6b451dab2d78273e352c42af78c871637c10f1ca
João Pedro Telini <joaotelini03@gmail.com>
2025-06-18 23:04:22 -0300

    Refactor TaskList component to improve task fetching, loading state handling, and update task display logic

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,73 +1,69 @@
 "use client";
+
 import { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { BadgeCheck, Trash2 } from "lucide-react";
+import { getTasksData } from "@/services/TaskApi";
 import { Task } from "@/types/TaskType";
-import { getTasksData } from "@/data/TaskData";
-import { Trash2, BadgeCheck } from "lucide-react";
+import { TaskInput } from "./TaskInput";
 
 export const TaskList = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const handleDeleteTask = (taskId: string) => {
-    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+  const fetchTasks = async () => {
+    setIsLoading(true);
+    const updatedTasks = await getTasksData();
     setTasks(updatedTasks);
-    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
-  };
-
-  const handleCompleteTask = (taskId: string) => {
-    const updatedTasks = tasks.map((task) => {
-      if (task.id === taskId) {
-        return { ...task, completed: !task.completed };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    setIsLoading(false);
   };
 
   useEffect(() => {
-    const updateTasks = () => {
-      setTasks(getTasksData());
-    };
-
-    // Pega a atualizacao enviada
-    window.addEventListener("tasksUpdated", updateTasks);
-    updateTasks();
-
-    return () => {
-      window.removeEventListener("tasksUpdated", updateTasks);
-    };
+    fetchTasks();
   }, []);
 
   return (
     <div className="mt-5 w-100 transition-all duration-300">
-      {tasks.length === 0 ? (
+      <TaskInput onAddTask={fetchTasks} />
+      {isLoading ? (
+        <ul className="flex flex-col gap-2 rounded-lg p-4 shadow-lg">
+          {Array.from({ length: 3 }).map((_, idx) => (
+            <li
+              key={idx}
+              className="flex items-center justify-between gap-5 px-4 py-2 rounded-lg bg-zinc-800"
+            >
+              <Skeleton className="h-4 w-3/4" />
+              <div className="flex items-center gap-3">
+                <Skeleton className="h-5 w-5 rounded-full animate-pulse" />
+                <Skeleton className="h-5 w-5 rounded-full animate-pulse " />
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : tasks.length === 0 ? (
         <p className="text-center">Nenhuma tarefa encontrada.</p>
       ) : (
-        <ul className="flex flex-col gap-2 rounded-lg p-4 shadow-lg">
+        <ul className="flex flex-col gap-2 rounded-lg p-4 shadow-lg transition-opacity duration-300">
           {tasks
-            .sort((a, b) => Number(a.completed) - Number(b.completed))
+            .sort((a, b) => Number(a.status) - Number(b.status))
             .map((task) => (
               <li
                 key={task.id}
                 className={`flex items-center justify-between gap-5 px-4 py-2 rounded-lg ${
-                  task.completed ? "bg-zinc-500" : "bg-zinc-800"
+                  task.status ? "bg-zinc-500" : "bg-zinc-800"
                 }`}
               >
-                <span
-                  className={`${task.completed ? "line-through italic" : ""}`}
-                >
-                  {task.taskName}
+                <span className={`${task.status ? "line-through italic" : ""}`}>
+                  {task.title}
                 </span>
 
                 <div className="flex items-center gap-3">
                   <BadgeCheck
-                    onClick={() => handleCompleteTask(task.id)}
                     className="w-5 h-5 cursor-pointer"
                     color="#3de1de"
                     strokeWidth={0.75}
                   />
                   <Trash2
-                    onClick={() => handleDeleteTask(task.id)}
                     className="w-5 h-5 cursor-pointer"
                     color="#bd2828"
                     strokeWidth={1.5}

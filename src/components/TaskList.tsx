ae43e9b13d4e88984032e803c14cee6aa1e955a5
João Pedro Telini <joaotelini03@gmail.com>
2025-06-21 00:09:20 -0300

    Add task deletion functionality with toast notifications for success and error handling

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -3,8 +3,9 @@
 import { useEffect, useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { BadgeCheck, Trash2 } from "lucide-react";
-import { getTasksData, setTaskCompleted } from "@/services/TaskApi";
+import { getTasksData, setTaskCompleted, deleteTask } from "@/services/TaskApi";
 import { Task } from "@/types/TaskType";
+import { toast } from "sonner";
 import { TaskInput } from "./TaskInput";
 
 export const TaskList = () => {
@@ -20,6 +21,12 @@ export const TaskList = () => {
 
   const handleSetCompleted = async (task: Task) => {
     await setTaskCompleted(task.id, !task.status);
+    fetchTasks();
+  };
+
+  const handleDeleteTask = async (task: Task) => {
+    await deleteTask(task.id);
+    console.log("task deletada com o id", task.id);
     fetchTasks();
   };
 
@@ -64,12 +71,61 @@ export const TaskList = () => {
 
                 <div className="flex items-center gap-3">
                   <BadgeCheck
-                    onClick={() => handleSetCompleted(task)}
+                    onClick={async () => {
+                      const newStatus = !task.status;
+                      await handleSetCompleted(task);
+
+                      const bgColor = newStatus
+                        ? "bg-green-600"
+                        : "bg-yellow-500";
+                      const textColor = newStatus ? "text-white" : "text-black";
+                      const message = newStatus
+                        ? "Tarefa marcada como concluída"
+                        : "Tarefa marcada como pendente";
+
+                      toast.custom((t) => (
+                        <div
+                          className={`${bgColor} ${textColor} px-6 py-4 rounded-lg shadow-lg flex flex-row items-center justify-center text-center w-full max-w-sm gap-2`}
+                          onClick={() => toast.dismiss(t)}
+                        >
+                          <BadgeCheck
+                            size={20}
+                            strokeWidth={1.5}
+                            className={textColor}
+                          />
+                          <span className="text-sm font-medium">{message}</span>
+                        </div>
+                      ));
+                    }}
                     className="w-5 h-5 cursor-pointer"
                     color="#3de1de"
                     strokeWidth={0.75}
                   />
+
                   <Trash2
+                    onClick={async () => {
+                      try {
+                        await handleDeleteTask(task);
+                        toast.custom((t) => (
+                          <div
+                            className="bg-red-600 text-white px-6 py-4 rounded-lg shadow-lg flex flex-row items-center justify-center text-center w-full max-w-sm gap-2"
+                            onClick={() => toast.dismiss(t)}
+                          >
+                            <BadgeCheck
+                              size={20}
+                              strokeWidth={1.5}
+                              className="text-white"
+                            />
+                            <span className="text-sm font-medium">
+                              Task removida com sucesso
+                            </span>
+                          </div>
+                        ));
+                      } catch (e) {
+                        toast.error("Erro ao excluir a tarefa.");
+                        console.error(e);
+                      }
+                    }}
                     className="w-5 h-5 cursor-pointer"
                     color="#bd2828"
                     strokeWidth={1.5}

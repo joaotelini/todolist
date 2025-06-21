dc64b8a0c121ea38729a7daf32e8fa2c23a208d8
João Pedro Telini <joaotelini03@gmail.com>
2025-06-20 23:50:45 -0300

    Add toast notification for successful task addition and enforce NewTask type in TaskInput

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -4,6 +4,9 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { useState } from "react";
 import { saveTasksData } from "@/services/TaskApi";
+import { toast } from "sonner";
+import { BadgeCheck } from "lucide-react";
+import { NewTask } from "@/types/TaskType";
 
 type TaskInputProps = {
   onAddTask: () => void;
@@ -19,7 +22,7 @@ export const TaskInput = ({ onAddTask }: TaskInputProps) => {
       return alert("Preencha o campo");
     }
 
-    const newTask = { title: taskName, status: false };
+    const newTask:NewTask = { title: taskName, status: false };
 
     try {
       await saveTasksData(newTask);
@@ -44,6 +47,23 @@ export const TaskInput = ({ onAddTask }: TaskInputProps) => {
         />
         <Button
           className="font-normal ml-5 w-20 p-4 cursor-pointer"
+          onClick={() => {
+            toast.custom((t) => (
+              <div
+                className="bg-zinc-800 text-white px-6 py-4 rounded-lg shadow-lg flex flex-row items-center justify-center text-center w-full max-w-sm gap-2"
+                onClick={() => toast.dismiss(t)}
+              >
+                <BadgeCheck
+                  size={20}
+                  strokeWidth={1.5}
+                  className="text-emerald-400"
+                />
+                <span className="text-sm font-medium">
+                  Task adicionada com sucesso
+                </span>
+              </div>
+            ));
+          }}
           type="submit"
           variant="default"
           disabled={!taskName.trim()}

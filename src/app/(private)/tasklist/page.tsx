a0ea936b22f5cbf870bd3dbda8d0023a296be5bd
João Pedro Telini <joaotelini03@gmail.com>
2025-06-26 22:56:46 -0300

    Add global styles and layout for task management with authentication

---
+++ src/app/(private)/tasklist/page.tsx
@@ -0,0 +1,37 @@
+import { TaskList } from "@/components/TaskList";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import jwt from "jsonwebtoken";
+
+const SECRET_KEY = process.env.SECRET;
+
+export default async function TaskListPrivate() {
+  console.log(SECRET_KEY);
+  if (!SECRET_KEY) {
+    throw new Error("SECRET_KEY não está definida");
+  }
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
+    redirect("/login");
+    return null;
+  }
+
+  try {
+    jwt.verify(token, SECRET_KEY);
+  } catch (error) {
+    redirect("/login");
+    return null;
+  }
+
+  return (
+    <div className="flex text-white min-h-screen items-center flex-col justify-center bg-gradient-to-br from-stone-800 via-zinc-700 to-slate-900 ">
+      <h1 className="mb-10 text-5xl font-bold text-center">Lista de Tarefas</h1>
+      <div>
+        <TaskList />
+      </div>
+    </div>
+  );
+}

bb7552993f3b3cdd640bd4a3d4008c505a89a5fa
João Pedro Telini <joaotelini03@gmail.com>
2025-06-26 22:56:38 -0300

    Add global styles and layout for login page

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center px-4 bg-gradient-to-br from-[#1a132d] via-[#180e3a] to-[#5836d3] text-white">
+      <LoginForm />
+    </main>
+  );
+}

8346324830f4b72efb42232f47ec216fa39ae5aa
João Pedro Telini <joaotelini03@gmail.com>
2025-06-27 23:40:52 -0300

    Refactor TaskType definitions for consistency and clarity

--- src/types/TaskType.ts
+++ src/types/TaskType.ts
@@ -1,7 +1,15 @@
-export type Task = {
+export type TaskType = {
   _id: string;
   title: string;
   status: boolean;
 };
 
-export type NewTask = Omit<Task, "_id" | "status">;
+export type NewTaskType = Omit<TaskType, "_id" | "status">;
+
+export type EditStatusTaskType = Omit<TaskType, "title">;
+
+export type TaskApiResponse = {
+  error: boolean;
+  message?: string;
+  data?: TaskType[];
+};

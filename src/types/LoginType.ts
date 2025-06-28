fbbcd4b18151ee1615f649359565cd6967a9210d
João Pedro Telini <joaotelini03@gmail.com>
2025-06-27 23:40:45 -0300

    Add LoginApiResponse type to enhance API response structure

--- src/types/LoginType.ts
+++ src/types/LoginType.ts
@@ -2,3 +2,10 @@ export type LoginType = {
   email: string;
   password: string;
 };
+
+export type LoginApiResponse = {
+  error: boolean;
+  token?: string;
+  message?: string;
+  data?: string;
+};

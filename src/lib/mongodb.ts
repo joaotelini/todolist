d7f2ac909b6ccad76ac5928971d8652c41881d1b
João Pedro Telini <joaotelini03@gmail.com>
2025-07-01 10:19:47 -0300

    Add MongoDB connection utility with global client promise

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+if (!uri) {
+  throw new Error("MONGODB_URI environment variable is not defined");
+}
+
+// Tipagem global
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, options);
+  global._mongoClientPromise = client.connect();
+}
+
+clientPromise = global._mongoClientPromise;
+
+export const connectDB = async () => {
+  const client = await clientPromise;
+  return client.db("todoList");
+};

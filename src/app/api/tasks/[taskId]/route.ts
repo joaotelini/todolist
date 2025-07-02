320573cd1da33beab7dc8d65e586e5465d85be2e
João Pedro Telini <joaotelini03@gmail.com>
2025-07-02 01:18:01 -0300

    Refactor task route handlers to use async functions for PATCH and DELETE methods

--- src/app/api/tasks/[taskId]/route.ts
+++ src/app/api/tasks/[taskId]/route.ts
@@ -1,4 +1,16 @@
 import { deleteTaskController, editTaskController } from "../controller";
+import { NextRequest } from "next/server";
 
-export const PATCH = editTaskController;
-export const DELETE = deleteTaskController;
+export async function PATCH(
+  request: NextRequest,
+  context: { params: { taskId: string } }
+) {
+  return editTaskController(request, context);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  context: { params: { taskId: string } }
+) {
+  return deleteTaskController(request, context);
+}

63d78e7e7dcd3d87be1e078e84332c911b48bc3b
João Pedro Telini <joaotelini03@gmail.com>
2025-07-02 01:25:10 -0300

    Refactor route.ts to improve type handling in PATCH and DELETE methods by using a Promise for params

--- src/app/api/tasks/[taskId]/route.ts
+++ src/app/api/tasks/[taskId]/route.ts
@@ -1,16 +1,16 @@
 import { deleteTaskController, editTaskController } from "../controller";
 import { NextRequest } from "next/server";
 
-type RouteParams = {
-  params: {
-    taskId: string;
-  };
+type RouteContext = {
+  params: Promise<{ taskId: string }>;
 };
 
-export async function PATCH(request: NextRequest, context: RouteParams) {
-  return editTaskController(request, context);
+export async function PATCH(request: NextRequest, context: RouteContext) {
+  const params = await context.params;
+  return editTaskController(request, { params });
 }
 
-export async function DELETE(request: NextRequest, context: RouteParams) {
-  return deleteTaskController(request, context);
+export async function DELETE(request: NextRequest, context: RouteContext) {
+  const params = await context.params;
+  return deleteTaskController(request, { params });
 }

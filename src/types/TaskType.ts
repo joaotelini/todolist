0451a7b5d07d9987167eb72d81518c36846d75e1
telinidev <joaotelini03@gmail.com>
2025-07-13 20:36:07 -0300

    Update TaskType to include description and category fields for improved task management

--- src/types/TaskType.ts
+++ src/types/TaskType.ts
@@ -1,12 +1,17 @@
 export type TaskType = {
   _id: string;
   title: string;
+  description: string;
+  category: string;
   status: boolean;
 };
 
 export type NewTaskType = Omit<TaskType, "_id" | "status">;
 
-export type EditStatusTaskType = Omit<TaskType, "title">;
+export type EditStatusTaskType = Omit<
+  TaskType,
+  "title" | "description" | "category"
+>;
 
 export type TaskApiResponse = {
   error: boolean;
